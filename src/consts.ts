9dcb3578a0cdf836ea5c8875aa96cc9eab19b686
Nik0-0 <maciek.f137@gmail.com>
2026-02-02 20:48:47 +0100

    💫 Small Refresh

--- src/consts.ts
+++ src/consts.ts
@@ -1,96 +1,108 @@
-import type { Site, Page, Links, Socials } from "@types"
+import type { Site, Page, Links, Socials } from "@types";
 
 // Global
 export const SITE: Site = {
   TITLE: "Pachbud",
   DESCRIPTION: "Usługi budowlane i remontowe",
   AUTHOR: "Niko's Stuff",
-}
+};
 
 // Work Page
 export const WORK: Page = {
   TITLE: "Work",
   DESCRIPTION: "Places I have worked.",
-}
+};
 
 // Blog Page
 export const BLOG: Page = {
   TITLE: "Blog",
   DESCRIPTION: "Nowości, aktualizacje, itp.",
-}
+};
 
-// Projects Page 
+// Projects Page
 export const PROJECTS: Page = {
   TITLE: "Projekty",
   DESCRIPTION: "Wykonane prace, projekty, itp.",
-}
+};
 
 // Team Page
 export const TEAM: Page = {
   TITLE: "Team",
   DESCRIPTION: "See our team.",
-}
+};
 
 // Other Page
 export const OTHERPROJECTS: Page = {
   TITLE: "Other Stuff",
   DESCRIPTION: "Random unfinished projects.",
-}
+};
 
 // Search Page
 export const SEARCH: Page = {
   TITLE: "Wyszukaj",
   DESCRIPTION: "Search all posts and projects by keyword.",
-}
+};
 
 // NNB Workshop
 export const NNB_WORKSHOP: Page = {
   TITLE: "NNB Workshop",
   DESCRIPTION: "Explore modded content.",
-}
+};
 
 export const DYNAMIC_PRICING: Page = {
   TITLE: "Kosztorys",
-  DESCRIPTION: "Szybki kosztorys usług.", 
-}
+  DESCRIPTION: "Szybki kosztorys usług.",
+};
 
 // Links
 export const LINKS: Links = [
-  { 
-    TEXT: "Główna", 
-    HREF: "/", 
+  {
+    TEXT: "Główna",
+    HREF: "/",
   },
-  // { 
+  // {
   //   TEXT: "Team",
   //   HREF: "/team",
   // },
-  { 
-    TEXT: "Blog", 
-    HREF: "/blog", 
-  },
-  { 
+  // {
+  //   TEXT: "Blog",
+  //   HREF: "/blog",
+  // },
+  {
     TEXT: "Projekty",
-    HREF: "/projects", 
+    HREF: "https://www.facebook.com/people/Pachbud-Us%C5%82ugi-Budowlane/100042866679570/",
   },
-  // { 
+  // {
   //   TEXT: "Mapy MC",
-  //   HREF: "/downloads", 
+  //   HREF: "/downloads",
   // },
   // {
   //   TEXT: "Galeria",
   //   HREF: "/galeria",
 
   // }
-]
+];
 
 // Socials
 export const SOCIALS: Socials = [
-  // {
-  //   NAME: "Email",
-  //   ICON: "email",
-  //   TEXT: "team@nikostuff.com",
-  //   HREF: "mailto:team@nikostuff.com",
-  // },
+  {
+    NAME: "Email",
+    ICON: "email",
+    TEXT: "franekpach@interia.pl",
+    HREF: "mailto:franekpach@interia.pl",
+  },
+  {
+    NAME: "Komórka",
+    ICON: "phone",
+    TEXT: "18 33 45 511",
+    HREF: "tel:18 33 45 511",
+  },
+  {
+    NAME: "Telefon",
+    ICON: "phone",
+    TEXT: "516 756 699",
+    HREF: "tel:+48 516 756 699",
+  },
   // {
   //   NAME: "Github",
   //   ICON: "github",
@@ -115,5 +127,4 @@ export const SOCIALS: Socials = [
   //   TEXT: "niko_fluffer",
   //   HREF: "https://dc.nikostuff.com",
   // }
-]
-
+];
